bc14b130fa16290a6c4c028d5d2faa0c3e748faf
Dobai Zsolt <dobaizsolt97@gmail.com>
2020-04-15 12:30:53 +0300

    initializing project

---
+++ utils/dummyData.js
@@ -0,0 +1,28 @@
+const dummyData = {
+  JavaScript: {
+    title: "JavaScript",
+    questions: [
+      {
+        question: "Can JavaScript be used to write backend code?",
+        answer: "Yes, using NodeJS",
+      },
+      {
+        question: "Can JavaScriot be used to write front end code?",
+        answer: "Yes, in fact it is the main language used on the web",
+      },
+    ],
+  },
+  CSS: {
+    title: "CSS",
+    questions: [
+      {
+        question: "Can you modify the color of a div using CSS?",
+        answer: "Yes",
+      },
+      {
+        question: "Cand you modify the color of a View with CSS?",
+        answer: "No, CSS is not supported in Native apps",
+      },
+    ],
+  },
+};
